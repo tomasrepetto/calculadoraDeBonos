1a8cf5b12b6db65e1a8aa0162b16b5fa3e0031ab
tomasrepetto <tomasrepetto@gmail.com>
2024-04-09 15:06:23 -0300

    Segundo dia

--- public/calculadora.js
+++ public/calculadora.js
@@ -15,3 +15,16 @@ document.getElementById('bondForm').addEventListener('submit', function(e) {
     e.preventDefault();
     document.getElementById('result').innerText = `Cálculo realizado. Verifica la consola para resultados.`;
 });
+
+document.getElementById('bondName').addEventListener('change', function() {
+    const selectedSymbol = this.value;
+    fetch('datos.json') // Asegúrate de que la ruta al archivo sea correcta.
+    .then(response => response.json())
+    .then(data => {
+        const selectedBond = data.find(bono => bono.SIMBOLO === selectedSymbol);
+        if (selectedBond) {
+            document.getElementById('tipoCambioTexto').innerText = `Tipo de cambio: ${selectedBond.MONEDA}`;
+        }
+    })
+    .catch(error => console.error('Error al cargar los bonos:', error));
+});
